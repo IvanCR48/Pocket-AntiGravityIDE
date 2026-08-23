fab84b1cf9e7fd1465fc69863d3dc4cc0835361d
IvanCR48 <cardozoivanismael@gmail.com>
2026-08-22 21:26:53 -0300

    refactor: clean up obsolete debug scripts, orphan binaries and unused assets

--- scripts/test-node-ps.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  const output = execSync('powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-Process | Select-Object -First 5 | ConvertTo-Json"', { encoding: 'utf8' });
-  console.log("PS Output:");
-  console.log(output);
-} catch (err) {
-  console.error("PS Error:", err);
-}
